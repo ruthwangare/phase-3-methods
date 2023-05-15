2f1725609cd6cb73e024c5aab2056221c7d8618c
Ruth <rwangare124@gmail.com>
2023-05-15 13:49:24 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def add(num1, num2)
+    num1 + num2
+   end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = Naureen)
+    puts "Hello, #{name}!"
+    end
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def halve(number)
+    return nil unless number.is_a?(Numeric)
+
+    number / 2
+  end
+
+  sum = add(1, 2)
+  puts sum
+
+  result = halve(4)
+  puts result
+  # => 2
+
+  result = halve("two")
+  puts result
+  # => nil
